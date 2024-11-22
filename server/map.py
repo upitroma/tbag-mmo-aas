bde4158a5e4aa0eaabf39bc10bd9e45b6b3b0e28
upitroma <55750919+upitroma@users.noreply.github.com>
2024-11-21 20:29:02 -0500

    map

---
+++ server/map.py
@@ -0,0 +1,50 @@
+rooms = {
+    0:{
+        "description":"a lovely campsite",
+        "paths":[1]
+    },
+    1:{
+        "description":"an open field",
+        "paths":[0,2,6]
+    },
+    2:{
+        "description":"a small patch of forest with an abandoned tent",
+        "paths":[1,3,4,6]
+    },
+    3:{
+        "description":"a rocky patch with a flowing waterfall",
+        "paths":[2,7]
+    },
+    4:{
+        "description":"a dense woods with a beehive near the treeline",
+        "paths":[2,5,6,7]
+    },
+    5:{
+        "description":"a cliff-side view of the village below",
+        "paths":[4,6,8]
+    },
+    6:{
+        "description":"a very large open field",
+        "paths":[1,2,4,5]
+    },
+    7:{
+        "description":"a small oasis",
+        "paths":[3,4,8]
+    },
+    8:{
+        "description":"a rocky cliff near a cave entrance",
+        "paths":[5,7,9]
+    },
+    9:{
+        "description":"a dark cave filled with small spiders",
+        "paths":[10,11]
+    },
+    10:{
+        "description":"a cove covered in vines with a stream leading over a ledge",
+        "paths":[3,9,11]
+    },
+    11:{
+        "description":"a dark with glowing, pulsing mushrooms at its center",
+        "paths":[9,10]
+    }
+}
